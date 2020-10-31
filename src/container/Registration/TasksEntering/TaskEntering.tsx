e60f4ea027fef74df6b630e08647e396a119be15
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2020-10-31 14:18:57 +0300

    feat: сверстан экран ввода задач

--- src/container/Registration/TasksEntering/TaskEntering.tsx
+++ src/container/Registration/TasksEntering/TaskEntering.tsx
@@ -1,7 +1,9 @@
 import React, {useCallback, useState} from 'react';
 import {observer} from 'mobx-react';
-import {Button, Text, TextInput, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {authStore} from '../../../store/Auth.store';
+import {CheckBox, Image, Input, Text} from 'react-native-elements';
+import {Button} from '../../../component/Button';
 
 export const TaskEntering = observer(() => {
   const [task, setTask] = useState('');
@@ -16,18 +18,76 @@ export const TaskEntering = observer(() => {
   }, []);
 
   return (
-    <View>
-      <Text>Вход</Text>
-      <TextInput
-        placeholder="Goal"
-        onChangeText={(text) => setTask(text)}
-        defaultValue={task}
+    <View style={styles.container}>
+      <View style={styles.controlsContainer}>
+        <Text h2 h2Style={styles.title}>
+          Распиши задачи
+        </Text>
+        <Input
+          containerStyle={styles.input}
+          placeholder={'Добавить задачу...'}
+          onChangeText={(text) => setTask(text)}
+          onEndEditing={() => addTask()}
+          defaultValue={task}
+        />
+        {tasks.map((aTask) => (
+          <CheckBox
+            key={aTask}
+            title={aTask}
+            checked={false}
+            containerStyle={styles.checkbox}
+            textStyle={styles.checkboxText}
+            size={33}
+          />
+        ))}
+        <Button
+          containerStyle={styles.button}
+          title={'Далее'}
+          onPress={submit}
+        />
+      </View>
+      <Image
+        source={require('../../../assets/fox-face.png')}
+        containerStyle={styles.image}
       />
-      {tasks.map((aTask) => (
-        <Text>{aTask}</Text>
-      ))}
-      <Button title={'Добавить'} onPress={addTask} />
-      <Button title={'Продолжить'} onPress={submit} />
     </View>
   );
+});
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexGrow: 1,
+    backgroundColor: '#ffffff',
+  },
+  controlsContainer: {
+    alignItems: 'center',
+  },
+  image: {
+    marginBottom: 40,
+    height: 152,
+    width: 209,
+  },
+  button: {
+    marginTop: 32,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '400',
+    marginTop: 16,
+    marginBottom: 32,
+  },
+  input: {
+    width: 250,
+  },
+  checkbox: {
+    borderWidth: 0,
+    backgroundColor: 'transparent',
+    width: 290,
+  },
+  checkboxText: {
+    fontWeight: '400',
+    fontSize: 18,
+  },
 });
